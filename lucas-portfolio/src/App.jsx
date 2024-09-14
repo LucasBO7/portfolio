6f840d2fa341ab5e66fa72dad8993562e7f981cd
Lucas Bianchezzi Oliveira <lucasbianchezzi700@gmail.com>
2024-09-14 11:42:39 -0300

    Criado projeto de portfolio e inserido e configurado tailwindcss

---
+++ lucas-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+      <p className="text-cyan-300">Bom dia</p>
+      <a className="border-lime-400 border text-blue-600" href="">
+        Banana
+      </a>
+    </>
+  );
+};
+
+export default App;
